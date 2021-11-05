f14909b9d8a2e2b091756e490af11024c4fa88db
David <david.c.dupuis@gmail.com>
2021-11-05 13:39:47 +0100

    adding trading view widget js

--- javascripts/landing.js
+++ javascripts/landing.js
@@ -146,3 +146,18 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+new TradingView.widget({
+  "width": 980,
+  "height": 610,
+  "symbol": "NASDAQ:AAPL",
+  "interval": "D",
+  "timezone": "Etc/UTC",
+  "theme": "light",
+  "style": "1",
+  "locale": "en",
+  "toolbar_bg": "#f1f3f6",
+  "enable_publishing": false,
+  "allow_symbol_change": true,
+  "container_id": "tradingview_27427"
+});
