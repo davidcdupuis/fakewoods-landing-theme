b6101e43935c10f535bf157de9d6598e1bf28458
David <david.c.dupuis@gmail.com>
2021-11-05 16:57:48 +0100

    tradingview fix

--- javascripts/landing.js
+++ javascripts/landing.js
@@ -147,17 +147,21 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
-new TradingView.widget({
-  "width": 980,
-  "height": 610,
-  "symbol": "NASDAQ:AAPL",
-  "interval": "D",
-  "timezone": "Etc/UTC",
-  "theme": "light",
-  "style": "1",
-  "locale": "en",
-  "toolbar_bg": "#f1f3f6",
-  "enable_publishing": false,
-  "allow_symbol_change": true,
-  "container_id": "tradingview_27427"
-});
+if (window.location.href.match(/\profile/))
+{
+  alert('profil page loaded! ');
+	new TradingView.widget({
+	  "width": 980,
+	  "height": 610,
+	  "symbol": "NASDAQ:AAPL",
+	  "interval": "D",
+	  "timezone": "Etc/UTC",
+	  "theme": "light",
+	  "style": "1",
+	  "locale": "en",
+	  "toolbar_bg": "#f1f3f6",
+	  "enable_publishing": false,
+	  "allow_symbol_change": true,
+	  "container_id": "tradingview_27427"
+	});
+}
