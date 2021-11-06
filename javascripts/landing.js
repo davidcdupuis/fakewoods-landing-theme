05ae399766df19f683a52fffb64deb9c4c61f38c
David <david.c.dupuis@gmail.com>
2021-11-06 12:51:34 +0100

    test

--- javascripts/landing.js
+++ javascripts/landing.js
@@ -147,20 +147,20 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
-if (window.location.href.match(/\profile/))
-{
-	new TradingView.widget({
-	  "width": 980,
-	  "height": 610,
-	  "symbol": "NASDAQ:AAPL",
-	  "interval": "D",
-	  "timezone": "Etc/UTC",
-	  "theme": "light",
-	  "style": "1",
-	  "locale": "en",
-	  "toolbar_bg": "#f1f3f6",
-	  "enable_publishing": false,
-	  "allow_symbol_change": true,
-	  "container_id": "tradingview_27427"
-	});
-}
+// if (window.location.href.match(/\profile/))
+// {
+// 	new TradingView.widget({
+// 	  "width": 980,
+// 	  "height": 610,
+// 	  "symbol": "NASDAQ:AAPL",
+// 	  "interval": "D",
+// 	  "timezone": "Etc/UTC",
+// 	  "theme": "light",
+// 	  "style": "1",
+// 	  "locale": "en",
+// 	  "toolbar_bg": "#f1f3f6",
+// 	  "enable_publishing": false,
+// 	  "allow_symbol_change": true,
+// 	  "container_id": "tradingview_27427"
+// 	});
+// }
